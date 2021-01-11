313805ab8b8d22dfa1191b4a781163af5bbfa654
joyrisen <68549204+joyrisen@users.noreply.github.com>
2021-01-12 02:45:36 +0300

    npm failed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,13 @@
 // Replace these credentials with your Firebase credentials
 
-export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_DOMAIN.firebaseapp.com",
-  databaseURL: "https://YOUR_DOMAIN.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID"
+export // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDMsPInM58uPksNu3E9iQ5EhnWBz67CPeQ",
+  authDomain: "scanpal-f74da.firebaseapp.com",
+  databaseURL: "https://scanpal-f74da.firebaseio.com",
+  projectId: "scanpal-f74da",
+  storageBucket: "scanpal-f74da.appspot.com",
+  messagingSenderId: "702443834173",
+  appId: "1:702443834173:web:eea09b202c6abcb5da1354",
+  measurementId: "G-9MVD7S5CEC"
 };
